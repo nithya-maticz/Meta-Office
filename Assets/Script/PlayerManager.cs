f94ba87f7cd4536edfa236262536ba7aa7992795
nithya-maticz <nithyamaticz@gmail.com>
2025-05-26 18:36:15 +0530

    commit

--- Assets/Script/PlayerManager.cs
+++ Assets/Script/PlayerManager.cs
@@ -7,11 +7,12 @@ public class PlayerManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     PhotonView PV;
-    
-
+    private Manager1 Manager;
     private void Awake()
     {
         PV = GetComponent<PhotonView>();
+        Manager = FindAnyObjectByType<Manager1>();
+        
     }
     void Start()
     {
@@ -26,11 +27,40 @@ public class PlayerManager : MonoBehaviour
     void CreateController()
     {
         Transform spawnPoint = SpawnManager.Instance.GetSpawnpoint();
-        Debug.Log("Spawn New Character " );
-       // Debug.Log("Gamemanager set: " + GameManager.Instance.spawnPoint);
+
+        Debug.Log(Manager.yourAvatarId);
+        switch(Manager.yourAvatarId)
+        {
+            case 1:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 1"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 2:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 2"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 3:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 3"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 4:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 4"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 5:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 5"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 6:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 6"), spawnPoint.position, spawnPoint.rotation);
+                break;
+            case 7:
+                PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController 7"), spawnPoint.position, spawnPoint.rotation);
+                break;
+
+
+        }
+
+        // Debug.Log("Gamemanager set: " + GameManager.Instance.spawnPoint);
         // PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), GameManager.Instance.spawnPoint.position, Quaternion.identity);
-       GameObject player =  PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position, Quaternion.identity);
-       // FindAnyObjectByType<Manager1>().players.Add(player.GetComponent<PlayerController>());
+
+        // GameObject player =  PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnPoint.position, spawnPoint.rotation, Quaternion.identity);
+        // FindAnyObjectByType<Manager1>().players.Add(player.GetComponent<PlayerController>());
     }
 
 
